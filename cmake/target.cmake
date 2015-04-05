a4d3d2b3c56ee5594475390755c7e6fa9641190d
Victor Holt <vqholt@gmail.com>
2015-04-04 22:10:25 -0500

    Added the libs folder to compile with an existing library

--- cmake/target.cmake
+++ cmake/target.cmake
@@ -3,4 +3,5 @@
 set(SOURCE_FILES main.cpp)
 
 add_executable(CMakeExample ${SOURCE_FILES})
+#target_link_libraries(CMakeExample ${PROJECT_SOURCE_DIR}/externals/libs/libMyLib.a)
 target_link_libraries(CMakeExample MyLib)
